fa34879e99a33aa5ba7b0daeba91cdee95aa36be
Poloveni <schricke.paul@gmail.com>
2026-07-08 15:21:49 +0200

    0

--- copy_to_clipboard.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-type "C:\Users\syne\Documents\GitHub\DépannagePcGard\clean_nav.txt" | clip
-echo Copie reussie dans le presse-papier!
-pause
--- setclip.ps1
+++
@@ -1,5 +0,0 @@
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$content = Get-Content -Path "$env:USERPROFILE\Documents\GitHub\DépannagePcGard\clean_nav.txt" -Raw -Encoding UTF8
-Set-Clipboard -Value $content
-Write-Host 'Done! Content length: ' + $content.Length
-pause
